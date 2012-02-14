c9231f335f50b5337be66ebaf4b01d0f3d817607
Dan Lazewatsky <dan@lazewatsky.com>
2012-02-14 21:50:59 +0000

    changes from PR2 testing

--- world_intersect/point_head.py
+++ world_intersect/point_head.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env rosh
 
 while ok():
-    goal = msg.pr2_controllers_msgs.PointHeadGoal(target=topics.intersected_point[0], min_duration=Duration(0.1))
+    goal = msg.pr2_controllers_msgs.PointHeadGoal(target=topics.intersected_point[0], min_duration=Duration(0.5))
     topics.head_traj_controller.point_head_action.goal(goal = goal)
     sleep(0.1)
 
