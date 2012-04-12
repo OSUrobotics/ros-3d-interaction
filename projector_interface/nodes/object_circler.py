54363e55fb8ccceeea5867c738b04a799bb5e975
Dan Lazewatsky <dan@lazewatsky.com>
2012-04-12 19:05:27 +0000

    green circles for selected object

--- projector_interface/nodes/object_circler.py
+++ projector_interface/nodes/object_circler.py
@@ -22,10 +22,14 @@ class Circler(QtGui.QWidget):
 	H = None
 	objects = None	
 	projected_objects = None
+	int_objects = None	
+	int_projected_objects = None
+	int_ages = []
 	model = None
 	camera_frame = None
 	object_header = None
 	object_lock = RLock()
+	intersected_lock = RLock()
 	
 	def escHandler(self, e):
 		sys.exit(0)
@@ -54,17 +58,34 @@ class Circler(QtGui.QWidget):
 			if objects.shape[1] == 0: return
 			self.object_header = msg.header
 			self.objects = objects
-			transformed_objects = self.projectPoints(self.objects)
+			transformed_objects = self.projectPoints(self.objects, msg.header)
 			self.projected_objects = cv2.perspectiveTransform(transformed_objects, self.H)
 		
+	def intersected_cb(self, msg):
+		#rospy.loginfo('internsected cb')
+		with self.intersected_lock:
+			objects = read_points_np(msg, masked=False)
+			print objects.shape
+			if objects.shape[1] == 0:
+				print objects
+				#self.int_objects = None 
+				#self.int_projected_objects = None
+				return
 
-	def projectPoints(self, points):
+			self.int_object_header = msg.header
+			self.int_objects = objects
+			transformed_objects = self.projectPoints(objects, msg.header)
+			self.int_projected_objects = cv2.perspectiveTransform(transformed_objects, self.H)
+			for pt in transformed_objects[0]:
+				self.int_ages.append(rospy.Time.now())
+
+	def projectPoints(self, points, point_header):
 		pts_out = []
 		for point in points[0]:
 			pt = PointStamped()
 			pt.point.x, pt.point.y, pt.point.z = point.tolist()
-			stamp = self.tfl.getLatestCommonTime(self.camera_frame, self.object_header.frame_id)
-			pt.header = self.object_header
+			stamp = self.tfl.getLatestCommonTime(self.camera_frame, point_header.frame_id)
+			pt.header = point_header
 			pt.header.stamp = stamp
 			# first, transform the point into the camera frame
 			pt_out = self.tfl.transformPoint(self.camera_frame, pt).point
@@ -78,11 +99,32 @@ class Circler(QtGui.QWidget):
 		with self.object_lock:
 			if self.objects is not None and self.projected_objects is not None:
 				for xformed in self.projected_objects[0]:
-					print 'circle at', xformed
+					#print 'circle at', xformed
 					r = 150
 					qp = QtGui.QPainter()
 					qp.begin(self)
 					color = QtGui.QColor(255,255,255)
+					qp.setPen(color)
+					pen = qp.pen()
+					pen.setWidth(5)
+					qp.setPen(pen)
+					rect = QtCore.QRectF(800-xformed[1]-r/2 + 25, 600-xformed[0]-r, r, r)
+					#rect = QtCore.QRectF(800-xformed[1]-r/2 + 25, 600-xformed[0]-r/2, r, r)
+					#rect = QtCore.QRectF(xformed[1]-r/2 + 25, xformed[0]-r/2, r, r)
+					qp.drawArc(rect, 0, 360*16)
+					qp.end()
+
+		#print '======================'
+		with self.intersected_lock:
+			if self.int_objects is not None and self.int_projected_objects is not None:
+				for (xformed, age) in zip(self.int_projected_objects[0], self.int_ages):
+					#if (rospy.Time.now() - age).to_sec() > 5: continue
+					#rospy.loginfo('intersected object at %s' % str(xformed))
+					#print 'green circle at', xformed
+					r = 100
+					qp = QtGui.QPainter()
+					qp.begin(self)
+					color = QtGui.QColor(0,255,0)
 					qp.setPen(color)
 					pen = qp.pen()
 					pen.setWidth(5)
@@ -104,9 +146,9 @@ class Circler(QtGui.QWidget):
 		print self.H
 		self.initUI()
 		rospy.Subscriber('object_cloud', PointCloud2, self.object_cb)
+		rospy.Subscriber('intersected_points', PointCloud2, self.intersected_cb)
 		rospy.Subscriber('camera_info', CameraInfo, self.info_cb)
 		timer = PySide.QtCore.QTimer(self)
-		#self.connect(timer, PySide.QtCore.SIGNAL('timeout()'), partial(self.publish_image, grid_pub))
 		timer.timeout.connect(self.update)
 		timer.start()
 		sys.exit(app.exec_())
