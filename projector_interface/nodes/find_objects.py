317b2f4a510b1875962a34bb54c88b1a978cada7
Dan Lazewatsky <dan@lazewatsky.com>
2012-04-18 22:29:34 +0000

    switched to using the tabletop segmentation service directly, and pr2_python for pointcloud conversions

--- projector_interface/nodes/find_objects.py
+++ projector_interface/nodes/find_objects.py
@@ -1,34 +1,37 @@
 #!/usr/bin/env python
 import roslib; roslib.load_manifest('projector_interface')
-from projector_interface._point_cloud import create_cloud_np_xyz
-from pr2_pick_and_place_demos.pick_and_place_manager import PickAndPlaceManager
+from tabletop_object_detector.srv import TabletopSegmentation, TabletopSegmentationRequest
+from pr2_python.pointclouds import xyz_array_to_pointcloud2
+
 import rospy
 import numpy as np
 from sensor_msgs.msg import PointCloud2
 
-def pt2Np(pt):
+def pt2np(pt):
     return np.array([pt.x,pt.y,pt.z])
 
-def detect(manager):
-    detection = manager.call_tabletop_detection(update_table=1, clear_attached_objects=1)
-    objects = detection[0]
+def cloud2np(cloud):
+	return np.asarray([pt2np(pt) for pt in cloud.points])		
+
+def location_from_cluster(cluster):
+	return cloud2np(cluster).mean(axis=0)
+
+def detect(detect_srv):
+    detection = detect_srv.call()
+    objects = detection.clusters
     header = rospy.Header()
     header.stamp = rospy.Time.now()
+    frame_id = ''
     if objects:
-        header.frame_id = objects[0].pose.header.frame_id
-    object_points = np.array([pt2Np(o.pose.pose.position) for o in objects])
-    object_cloud = create_cloud_np_xyz(np.float32(object_points), header)
-    return object_cloud
-    
+        frame_id = objects[0].header.frame_id
+    return xyz_array_to_pointcloud2(np.asarray([location_from_cluster(obj) for obj in objects]), stamp=rospy.Time.now(), frame_id=frame_id)
 
 if __name__ == '__main__':
     rospy.init_node('find_objects')
-    manager = PickAndPlaceManager()
-    # rate = rospy.Rate(5)
     object_pub = rospy.Publisher('object_cloud', PointCloud2)
+    detect_srv = rospy.ServiceProxy('/tabletop_segmentation', TabletopSegmentation)
+    detect_srv.wait_for_service()
     while not rospy.is_shutdown():
-        object_cloud = detect(manager)
-        # print object_cloud
+        object_cloud = detect(detect_srv)
         object_pub.publish(object_cloud)
         print 'published'
-        # rate.sleep()
