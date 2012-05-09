96c8d2c06bac496cb790ec9b8a84e2656a5933f0
Dan Lazewatsky <dan@lazewatsky.com>
2012-05-09 18:28:49 +0000

    dynamic reconfigure client to adjust the table cloud

--- projector_interface/nodes/find_objects.py
+++ projector_interface/nodes/find_objects.py
@@ -2,9 +2,10 @@
 import roslib; roslib.load_manifest('projector_interface')
 from tabletop_object_detector.srv import TabletopSegmentation, TabletopSegmentationRequest
 from pr2_python.pointclouds import xyz_array_to_pointcloud2
-from pr2_python.transform_listener import transform_point_stamped
+from pr2_python.transform_listener import transform_point_stamped, transform_pose_stamped
 from tf import TransformBroadcaster
 import tf_conversions.posemath as pm
+import dynamic_reconfigure.client
 from threading import Thread, RLock
 
 import rospy
@@ -35,7 +36,6 @@ def location_from_cluster(cluster):
     except tf.Exception as e:
         print 'exception...'
         return np.array([np.nan]*3)
-    # return cloud2np(cluster).mean(axis=0)
 
 def detect(detect_srv):
     detection = detect_srv.call()
@@ -44,18 +44,37 @@ def detect(detect_srv):
     header.stamp = rospy.Time.now()
     if objects:
         frame_id = objects[0].header.frame_id
-        # cloud = xyz_array_to_pointcloud2(np.asarray([location_from_cluster(obj) for obj in objects]), stamp=rospy.Time.now(), frame_id=frame_id)
         cloud = xyz_array_to_pointcloud2(np.asarray([location_from_cluster(obj) for obj in objects]), stamp=rospy.Time.now(), frame_id='/table')
-        return cloud, detection.table.pose
+        return cloud, detection.table
     return None, None
 
-# def broadcast_table_frame(table_pose, br, stamp=None):
 def broadcast_table_frame(args):
     if table_pose is None: return
     with tf_lock:
         trans, rot = pm.toTf(pm.fromMsg(table_pose.pose))
-        # if stamp is None: stamp = rospy.Time.now()
         br.sendTransform(trans, rot, rospy.Time.now(), '/table', table_pose.header.frame_id)
+
+def set_table_filter_limits(table, clients):
+    for field, client in clients.iteritems():
+        params = dict(
+            filter_field_name = field
+        )
+        if field in ('x','y'):
+            # params['input_frame'] = table.pose.header.frame_id
+            params['input_frame']  = '/table'
+            params['output_frame'] = '/table'
+            
+            params['filter_limit_min'] = table.__getattribute__('%s_min' % field)
+            params['filter_limit_max'] = table.__getattribute__('%s_max' % field)
+        else:
+            params['input_frame']  = '/table'
+            params['output_frame'] = '/table'
+            params['filter_limit_min'] = -0.01
+            params['filter_limit_max'] =  0.01
+            # params['filter_limit_min'] = -5
+            # params['filter_limit_max'] =  5
+        client.update_configuration(params)
+    
 
 if __name__ == '__main__':
     rospy.init_node('find_objects')
@@ -63,13 +82,16 @@ if __name__ == '__main__':
     br = TransformBroadcaster()	
     detect_srv = rospy.ServiceProxy('/tabletop_segmentation', TabletopSegmentation)
     detect_srv.wait_for_service()
-    print 'starting timer'
     Thread(target=rospy.Timer(rospy.Duration(0.05), broadcast_table_frame).run)
-    print 'started timer'
+    clients = dict(
+        x = dynamic_reconfigure.client.Client('xfilter', timeout=float('inf')),
+        y = dynamic_reconfigure.client.Client('yfilter', timeout=float('inf')),
+        z = dynamic_reconfigure.client.Client('zfilter', timeout=float('inf'))
+    )
     while not rospy.is_shutdown():
-        object_cloud, table_pose_tmp = detect(detect_srv)
+        object_cloud, table = detect(detect_srv)
         if object_cloud is not None:
             with tf_lock:
-                table_pose = table_pose_tmp
-            # broadcast_table_frame(table_pose, br, stamp=object_cloud.header.stamp)
+                table_pose = table.pose
+            set_table_filter_limits(table, clients)
             object_pub.publish(object_cloud)
