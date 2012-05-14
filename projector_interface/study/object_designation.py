139487d8e96382388fedfb7253121964a43e0826
Dan Lazewatsky <dan@lazewatsky.com>
2012-05-14 18:40:24 +0000

    layout objects, and collect data

--- projector_interface/study/object_designation.py
+++ projector_interface/study/object_designation.py
@@ -1,22 +1,46 @@
 #!/usr/bin/env python
 import roslib; roslib.load_manifest('projector_interface')
 from std_msgs.msg import ColorRGBA
-from pr2_python.pointclouds import pointcloud2_to_xyz_array
+from pr2_python.pointclouds import pointcloud2_to_xyz_array, xyz_array_to_pointcloud2
+import datetime
+import scipy.io
+import numpy as np
 
 load('rosh_robot', globals())
 
-points = [
-    [-0.5365211367607117,   0.2706190347671509,     0.0],
-    [-0.7820538282394409,   0.29613807797431946,    0.0],
-    [-0.21059279143810272,  0.267086386680603,      0.0],
-    [-0.2007334679365158,  -0.010890659876167774,   0.0],
-    [0.05050656571984291,   0.31135380268096924,    0.0],
-    [-0.5381582379341125,   0.512862503528595,      0.0],
-    [-0.5768512487411499,  -0.011733915656805038,   0.0],
-    [-0.2181294560432434,   0.5227704048156738,     0.0]
+xmin = -0.79651666
+xmax =  0.06501853
+ymin =  0.03261997
+ymax =  0.50817382
+
+# xs = np.linspace(xmin,xmax,4)
+ys = np.linspace(ymin,ymax,3)
+xs = np.arange(xmin+0.05, xmax, ys[1] - ys[0])
+xx, yy = np.meshgrid(xs, ys)
+points = np.array([(x,y,0) for x,y in zip(xx.ravel(), yy.ravel())])
+points_msg = xyz_array_to_pointcloud2(points, now(), '/table')
+topics.object_cloud(points_msg)
+
+def sameObject(self, p1, p2):
+    return self.dist(p1,p2) < SAME_OBJ_THRESH
+    
+order = [
+    0,  2,  1,  3,
+    # 7,  5,  6,  4,
+    # 8, 10,  9, 11,
+    # 0,  4,  8,
+    # 1,  5,  9,
+    # 2,  6,  10,
+    # 3,  7,  11
 ]
 
-for point in points:
+targets = []
+stds = []
+
+# for point in points:
+for idx in order:
+    point = points[idx]
+    targets.append(point)
     print 'hilighting ', point
     services.clear_hilights()
     
@@ -29,8 +53,12 @@ for point in points:
     # click_pts_msg = topics.click_stats[0]
     click_pts_msg = services.get_cursor_stats().points
     click_pts = pointcloud2_to_xyz_array(click_pts_msg)
-    print 'std = ', click_pts.std(0)
+    stds.append(click_pts.std(0))
+    print 'std = ', stds[-1]
     sleep(3)
-    
-    
+        
 services.clear_hilights()
+path = '/home/robotics/lazewatskyd/ros-pkgs/wu-ros-pkg/3d_interaction/projector_interface/study/data/user_%s.mat'
+timestr = datetime.datetime.today().isoformat()
+scipy.io.savemat(path % timestr, dict(targets=targets, stds=stds))
+print 'Saved %s' % (path % timestr)
