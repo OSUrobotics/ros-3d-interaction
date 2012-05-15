d4619bc63e5838766fbc68c97a8d8f2753a9327b
Dan Lazewatsky <dan@lazewatsky.com>
2012-05-15 21:38:40 +0000

    easer reordering of the sequences, and more data

--- projector_interface/study/object_designation.py
+++ projector_interface/study/object_designation.py
@@ -5,7 +5,7 @@ from pr2_python.pointclouds import pointcloud2_to_xyz_array, xyz_array_to_pointc
 import datetime
 import scipy.io
 import numpy as np
-
+from itertools import chain
 load('rosh_robot', globals())
 
 xmin = -0.79651666
@@ -25,20 +25,22 @@ def sameObject(self, p1, p2):
     return self.dist(p1,p2) < SAME_OBJ_THRESH
     
 order = [
-    0,  2,  1,  3,
-    # 7,  5,  6,  4,
-    # 8, 10,  9, 11,
-    # 0,  4,  8,
-    # 1,  5,  9,
-    # 2,  6,  10,
-    # 3,  7,  11
+    [0,  2,  1,  3],
+    [7,  5,  6,  4],
+    [8, 10,  9, 11],
+    [0,  4,  8 ],
+    [1,  5,  9 ],
+    [2,  6,  10],
+    [3,  7,  11]
 ]
 
 targets = []
 stds = []
+times = []
+cursor_history = []
+history_size = 0
 
-# for point in points:
-for idx in order:
+for idx in chain(order):
     point = points[idx]
     targets.append(point)
     print 'hilighting ', point
@@ -49,16 +51,25 @@ for idx in order:
     pt.header.stamp = now()
     pt.point.x, pt.point.y, pt.point.z = point
     
+    t1 = now()
     services.hilight_object(pt, ColorRGBA(255,0,0,0))
     # click_pts_msg = topics.click_stats[0]
     click_pts_msg = services.get_cursor_stats().points
+    t2 = now()
+    times.append((t2-t1).to_sec())
     click_pts = pointcloud2_to_xyz_array(click_pts_msg)
+    history_size = click_pts.shape[0]
+    cursor_history.extend(click_pts)
     stds.append(click_pts.std(0))
     print 'std = ', stds[-1]
     sleep(3)
         
 services.clear_hilights()
-path = '/home/robotics/lazewatskyd/ros-pkgs/wu-ros-pkg/3d_interaction/projector_interface/study/data/user_%s.mat'
-timestr = datetime.datetime.today().isoformat()
-scipy.io.savemat(path % timestr, dict(targets=targets, stds=stds))
+path = '/home/robotics/lazewatskyd/ros-pkgs/wu-ros-pkg/3d_interaction/projector_interface/study/data/object_designation_%s.mat'
+timestr = datetime.datetime.today().strftime('%d-%m-%y-%H.%M.%f')
+scipy.io.savemat(path % timestr,    dict(targets=targets,
+                                         stds=stds,
+                                         times=times,
+                                         cursor_history=cursor_history,
+                                         history_size=history_size))
 print 'Saved %s' % (path % timestr)
