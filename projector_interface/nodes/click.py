2efdb7a6621ee8df87f7cbc075f6df2e870da4ee
Dan Lazewatsky <dan@lazewatsky.com>
2012-05-25 21:48:00 +0000

    set a longer repeat time

--- projector_interface/nodes/click.py
+++ projector_interface/nodes/click.py
@@ -4,16 +4,20 @@ from sensor_msgs.msg import Joy
 from std_msgs.msg import Empty
 import rospy
 
-new_click = True
+click_inhibit = False
+
+def uninhibit(args):
+    global click_inhibit
+    click_inhibit = False
 
 def joy_cb(msg, args):
-    global new_click
+    global click_inhibit
     click = msg.buttons[2]
-    if click and new_click:
-        new_click = False
+    if click and not click_inhibit:
+        click_inhibit = True
         args[0].publish()
-    elif not click:
-        new_click = True
+        rospy.Timer(rospy.Duration(1.0), uninhibit, oneshot=True)
+		
 
 if __name__ == '__main__':
     rospy.init_node('point_to_click')
