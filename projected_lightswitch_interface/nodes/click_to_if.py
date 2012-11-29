382cb3fe59f641c7d7b6e9d4ad51bc52b47276bb
Dan Lazewatsky <dan@lazewatsky.com>
2012-11-29 17:08:22 -0600

    fixed an issue making it hard to compose the polygons in non-optical frames

--- projected_lightswitch_interface/nodes/click_to_if.py
+++ projected_lightswitch_interface/nodes/click_to_if.py
@@ -4,13 +4,19 @@ import rospy
 import tf
 from geometry_msgs.msg import Point, PointStamped, PolygonStamped
 from pr2_object_manipulation_msgs.msg import ImageClick
-from std_msgs.msg import ColorRGBA
+from std_msgs.msg import ColorRGBA, String
 from projector_interface.srv import GetCursorStats, DrawPolygon, CircleInhibit
 from copy import deepcopy
+
+
+polygon_viz = None
+
+
 tfl = None
 poly_proxy = None
 hilight_proxy = None
 clear_hilight_proxy = None
+inited = False
 
 SWITCH_WIDTH  = 0.08
 SWITCH_HEIGHT = 0.125
@@ -19,7 +25,13 @@ BUTTON_HEIGHT = BUTTON_WIDTH
 
 WHITE  = ColorRGBA(255,255,255,0)
 PURPLE = ColorRGBA(255,0  ,255,0)
-HILIGHT_COLOR = PURPLE
+#HILIGHT_COLOR = PURPLE
+HILIGHT_COLOR = WHITE
+
+poly_switch = None
+poly_on = None
+poly_off = None
+
 
 polygons = dict()
 
@@ -32,14 +44,23 @@ def rectangle(header, width, height, center):
 	# center = tfl.transformPoint('table', center_stamped).point
 	x,y,z = center.x,center.y,center.z
 
-	poly.polygon.points.append(Point(x,y-width/2,z-height/2))
-	poly.polygon.points.append(Point(x,y+width/2,z-height/2))
-	poly.polygon.points.append(Point(x,y+width/2,z+height/2))
-	poly.polygon.points.append(Point(x,y-width/2,z+height/2))
+	# this is for doing it in image coordinates
+	# poly.polygon.points.append(Point(x,y-width/2,z-height/2))
+	# poly.polygon.points.append(Point(x,y+width/2,z-height/2))
+	# poly.polygon.points.append(Point(x,y+width/2,z+height/2))
+	# poly.polygon.points.append(Point(x,y-width/2,z+height/2))
+
+	# normal frame alignment
+	poly.polygon.points.append(Point(x-width/2,y-height/2,z))
+	poly.polygon.points.append(Point(x+width/2,y-height/2,z))
+	poly.polygon.points.append(Point(x+width/2,y+height/2,z))
+	poly.polygon.points.append(Point(x-width/2,y+height/2,z))
+
 
 	return poly
 
 def click_cb(msg):
+	global poly_switch, poly_on, poly_off
 	poly_switch = rectangle(msg.ray.header, SWITCH_WIDTH, SWITCH_HEIGHT, msg.ray.direction)
 	on_center   = deepcopy(msg.ray.direction)
 	on_center.y += SWITCH_WIDTH/2 + BUTTON_WIDTH/2
@@ -57,12 +78,34 @@ def click_cb(msg):
 
 	# rospy.loginfo("Sent polygon %s" % str(poly))
 	polygon_proxy("Light\nSwitch", True, poly_switch, WHITE)
-	polygon_proxy("Turn On", True, poly_on, WHITE)
-	polygon_proxy("Turn Off", True, poly_off, WHITE)
+	print 'published viz'
+	# import pdb; pdb.set_trace()
+	polygon_viz.publish(poly_switch)
+
+	print poly_switch
+	if inited:
+		polygon_proxy("Turn On", True, poly_on, WHITE)
+		polygon_proxy("Turn Off", True, poly_off, WHITE)
 	rospy.loginfo("Sent polygons")
 
 def if_click_cb(msg):
+	rospy.loginfo('if click cb')
+	global inited
 	polygon_proxy(msg.data, True, polygons[msg.data], HILIGHT_COLOR)
+	print msg.data
+	if msg.data == "Light\nSwitch":
+		inited = True
+		polygon_proxy("Turn On", True, poly_on, WHITE)
+		polygon_proxy("Turn Off", True, poly_off, WHITE)
+
+def outline_switch():
+	fake_click = ImageClick()
+	fake_click.ray.header.frame_id = '/4x4_0'
+	fake_click.ray.header.stamp = rospy.Time.now()
+	fake_click.ray.direction.x = -0.11
+	fake_click.ray.direction.y =  0.0
+	fake_click.ray.direction.z =  0.0
+	click_cb(fake_click)
 
 if __name__ == '__main__':
 	rospy.init_node('click_to_if')
@@ -71,6 +114,8 @@ if __name__ == '__main__':
 	rospy.loginfo("Waiting for polygon service")
 	polygon_proxy.wait_for_service()
 	rospy.loginfo("polygon service ready")
+
+	polygon_viz = rospy.Publisher('/polygon_viz', PolygonStamped)
 	
 	# rospy.loginfo("Waiting for hilight service")
 	# hilight_proxy = rospy.ServiceProxy("hilight_object")
@@ -78,6 +123,7 @@ if __name__ == '__main__':
 	# rospy.loginfo("waiting for clear hilight service")
 	# clearhilight_proxy = rospy.ServiceProxy("clear_hilights")
 	# rospy.loginfo("clear hilight service ready")
-
-	rospy.Subscriber('/interactive_manipulation_image_click', ImageClick, click_cb)
+	outline_switch()
+	# rospy.Subscriber('/interactive_manipulation_image_click', ImageClick, click_cb)
+	# rospy.Subscriber('/clicked_object', String, if_click_cb)
 	rospy.spin()
