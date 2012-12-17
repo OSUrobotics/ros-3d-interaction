3914acd668ae3558a0e728770e752f6e371fe235
Dan Lazewatsky <dan@lazewatsky.com>
2012-12-17 10:11:58 -0600

    wiimote button

--- projector_interface/nodes/click.py
+++ projector_interface/nodes/click.py
@@ -12,7 +12,7 @@ def uninhibit(args):
 
 def joy_cb(msg, args):
     global click_inhibit
-    click = msg.buttons[16]
+    click = msg.buttons[2]
     if click and not click_inhibit:
         click_inhibit = True
         args[0].publish()
